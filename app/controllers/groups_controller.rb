9336943f056fd145ece7131dea8f4fab7f078385
马小舟 <maxiaozhou.kelly@gmail.com>
2017-04-13 10:21:55 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,6 +30,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path, notice: "上传成功"
   end
 
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "讨论组已删除"
+    redirect_to groups_path
+  end
+
   private
 
   def group_params
