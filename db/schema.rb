b3d32e9b5e0f1a69fa72705adbe7e6dc177296ee
马小舟 <maxiaozhou.kelly@gmail.com>
2017-04-14 21:11:31 +0800

    shou group's creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414070000) do
+ActiveRecord::Schema.define(version: 20170414124925) do
 
   create_table "grouds", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170414070000) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
