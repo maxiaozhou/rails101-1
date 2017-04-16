f7b0300d165f593c1fc0d1aa27734015a513cb5a
马小舟 <maxiaozhou.kelly@gmail.com>
2017-04-16 18:26:40 +0800

    post edit and destory

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "上传成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "文章已删除"
+    redirect_to account_posts_path
+  end
+
   private
 
   def post_params
